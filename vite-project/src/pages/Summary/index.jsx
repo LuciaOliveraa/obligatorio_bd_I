edd049ec9c825e57c5c3cb2ea0b0f180237e3d3b
JBevc <141636441+JBevc@users.noreply.github.com>
2024-11-08 15:23:23 -0300

    summary

--- vite-project/src/pages/Summary/index.jsx
+++ vite-project/src/pages/Summary/index.jsx
@@ -1,3 +1,13 @@
+import Navbar from "../../components/NavBar";
+import "./style.css";
+
 export default function Summary() {
-  return <></>;
+  return (
+    <div className="summary">
+      <Navbar></Navbar>
+      <div className="content">
+        <h1 className="title is-1">Resumen de su inscripción</h1>
+      </div>
+    </div>
+  );
 }
