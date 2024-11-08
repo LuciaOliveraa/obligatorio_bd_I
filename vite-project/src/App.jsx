7705a719b5b232fe5023918301164c1bd0bf0335
JBevc <141636441+JBevc@users.noreply.github.com>
2024-11-08 12:29:38 -0300

    cambios

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,31 +1,33 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
-import Login from '../pages/Login'
-import Administration from '../src/pages/Administration'
-import Equipment from '../src/pages/Equipment'
-import HomePage from '../src/pages/HomePage'
-import Schedule from '../src/pages/Schedule'
-import Summary from '../src/pages/Summary'
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Login from "..src/pages/Login";
+import Administration from "../src/pages/Administration";
+import Equipment from "../src/pages/Equipment";
+import HomePage from "../src/pages/HomePage";
+import Schedule from "../src/pages/Schedule";
+import Summary from "../src/pages/Summary";
 
 function App() {
-  
-
   return (
     <>
       <Router>
-        <div className='app'>
-          <Route path="/*" element={<Navigate replace to="/login"/>} />
-          <Route path='/login' element={<Login></Login>}/>
-          <Route path='/home' element={<HomePage></HomePage>}/>
-          <Route path='/schedule' element={<Schedule></Schedule>}/>
-          <Route path='/equipment' element={<Equipment></Equipment>}/>
-          <Route path='/summary' element={<Summary></Summary>}/>
-          <Route path='/admin' element={<Administration></Administration>}/>
+        <div className="app">
+          <Route path="/*" element={<Navigate replace to="/login" />} />
+          <Route path="/login" element={<Login></Login>} />
+          <Route path="/home" element={<HomePage></HomePage>} />
+          <Route path="/schedule" element={<Schedule></Schedule>} />
+          <Route path="/equipment" element={<Equipment></Equipment>} />
+          <Route path="/summary" element={<Summary></Summary>} />
+          <Route path="/admin" element={<Administration></Administration>} />
         </div>
       </Router>
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
