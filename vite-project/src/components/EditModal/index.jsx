4a628df373ff7b4f346345377f39c4ffc62b5eda
JBevc <141636441+JBevc@users.noreply.github.com>
2024-11-15 19:03:08 -0300

    modal edit

--- vite-project/src/components/EditModal/index.jsx
+++ vite-project/src/components/EditModal/index.jsx
@@ -5,6 +5,33 @@ export function EditModal() {
     <div className={style.modal}>
       <div className={style.modalContent}>
         <h1 className={style.title}> Editar alumno</h1>
+        <div className={style.editInfo}>
+          <div className={style.formRow}>
+            <div className={style.form}>
+              <label for="name"> Nombre</label>
+              <input type="text" id="name"></input>
+            </div>
+            <div className={style.form}>
+              <label for="surname"> Apellido</label>
+              <input type="text" id="surname"></input>
+            </div>
+          </div>
+
+          <div className={style.formRow}>
+            <div className={style.form}>
+              <label for="cedIdentidad"> Email</label>
+              <input type="email" id="cedIdentidad"></input>
+            </div>
+            <div className={style.form}>
+              <label for="bornDate"> Telefono</label>
+              <input type="numer" id="bornDate"></input>
+            </div>
+          </div>
+          <div className={style.saveCancel}>
+            <button className={style.cancelEdit}>Cancelar </button>
+            <button className={style.saveEdit}>Guardar cambios </button>
+          </div>
+        </div>
       </div>
     </div>
   );
