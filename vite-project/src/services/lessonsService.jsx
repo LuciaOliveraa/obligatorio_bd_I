f5e623e3bb96ae69952e9eb7267432c0e4644b2b
belentellechea <belen.tellechea@correo.ucu.edu.uy>
2024-11-18 22:49:52 -0300

    fetches

---
+++ vite-project/src/services/lessonsService.jsx
@@ -0,0 +1,50 @@
+
+export const getLessons = async() => {
+    try {
+        const response = await fetch (`http://localhost:8080/lessons`, {
+            method: "GET"
+        }) 
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.log('Error obteniendo clases', error)
+    }
+}
+
+export const getLesson = async(id) => {
+    try {
+        const response = await fetch (`http://localhost:8080/lessons/${id}`, {
+            method: "GET"
+        }) 
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.log('Error obteniendo clase por id', error)
+    }
+}
+
+export const getLessonByActivity = async(id) => {
+    try {
+        const response = await fetch (`http://localhost:8080/lessons/activity/${id}`, {
+            method: "GET"
+        }) 
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.log('Error obteniendo turno clase por actividad', error)
+    }
+}
+
+export const editLesson = async(id, editedLesson) => {
+    try {
+        const response = await fetch (`http://localhost:8080/activities/edit/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(editedLesson)
+        }) 
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.log('Error editando clase', error)
+    }
+}
