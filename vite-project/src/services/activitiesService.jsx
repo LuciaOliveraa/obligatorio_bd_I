c8fd8ee55d3004d945d8e68f67e3fe0d63d34b13
belentellechea <belen.tellechea@correo.ucu.edu.uy>
2024-11-18 22:26:42 -0300

    shift fetches

--- vite-project/src/services/activitiesService.jsx
+++ vite-project/src/services/activitiesService.jsx
@@ -12,31 +12,29 @@ export const getActivities = async () => {
     }
 }
 
-export const getActivity = async() => {
+export const getActivity = async(id) => {
     try {
-        const response = await fetch ("http://localhost:8080/activities/:id", {
+        const response = await fetch (`http://localhost:8080/activities/${id}`, {
             method: "GET"
         }) 
         const data = await response.json();
         return data; 
 
     } catch (error) {
-        console.log('Error obteniendo actividades', error)
+        console.log('Error obteniendo actividad por id', error)
     }
 }
 
-export const editActivity = async(editedActivity) => {
+export const editActivity = async(id, editedActivity) => {
     try {
-        const response = await fetch ("http://localhost:8080/activities/edit/:id", {
+        const response = await fetch (`http://localhost:8080/activities/edit/${id}`, {
             method: "PUT",
-            body: {
-                editedActivity
-            }
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(editedActivity)
         }) 
         const data = await response.json();
         return data; 
-
     } catch (error) {
-        console.log('Error obteniendo actividades', error)
+        console.log('Error editando actividad', error)
     }
 }
---
+++ vite-project/src/services/shiftsService.jsx
@@ -0,0 +1,66 @@
+
+export const getShifts = async() => {
+    try {
+        const response = await fetch (`http://localhost:8080/shifts`, {
+            method: "GET"
+        }) 
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.log('Error obteniendo turnos', error)
+    }
+}
+
+export const getShift = async(id) => {
+    try {
+        const response = await fetch (`http://localhost:8080/shifts/${id}`, {
+            method: "GET"
+        }) 
+        const data = await response.json();
+        return data; 
+
+    } catch (error) {
+        console.log('Error obteniendo turno por id', error)
+    }
+}
+
+export const addShift = async(newShift) => {
+    try {
+        const response = await fetch (`http://localhost:8080/shifts/add`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(newShift)
+        }) 
+        const data = await response.json();
+        return data; 
+
+    } catch (error) {
+        console.log('Error añadiendo nuevo turno', error)
+    }
+}
+
+export const deleteShift = async(id) => {
+    try {
+        const response = await fetch (`http://localhost:8080/shifts/delete/${id}`, {
+            method: "DELETE"
+        }) 
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.log('Error eliminando turno', error)
+    }
+}
+
+export const editShift = async(id, editedShift) => {
+    try {
+        const response = await fetch (`http://localhost:8080/activities/edit/${id}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(editedShift)
+        }) 
+        const data = await response.json();
+        return data; 
+    } catch (error) {
+        console.log('Error editando turno', error)
+    }
+}
