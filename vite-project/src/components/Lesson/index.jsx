58b438321ff95b688fab380aa295984d59524167
belentellechea <belen.tellechea@correo.ucu.edu.uy>
2024-11-24 17:21:26 -0300

    problema get por ci instructor

--- vite-project/src/components/Lesson/index.jsx
+++ vite-project/src/components/Lesson/index.jsx
@@ -1,6 +1,8 @@
 import style from "./Lesson.module.css";
 import { TbPencil as Pencil } from "react-icons/tb";
 import { GoTrash as Trash } from "react-icons/go";
+import { getInstructor } from "../../services/instructorsService";
+import { useState, useEffect } from "react";
 
 export default function Lesson({
   instructorId,
@@ -8,6 +10,23 @@ export default function Lesson({
   shiftId,
   capacity
 }) {
+  const [instructor, setInstructor] = useState({}); 
+
+  const fetchInstructor = async () => {
+    try {
+        const data = await getInstructor(instructorId); 
+        // setInstructor(data.instructor); 
+        console.log("instructor info", data)
+    } catch (error) {
+        console.error("Error obteniendo instructor", error)
+    }
+  }
+
+  useEffect(() => {
+    fetchInstructor();
+  }, []);
+
+
   return (
     <div className={style.infoandbuttons}>
       <div className={style.info}>
